1cacc9e16f9d0ed2489472d1b8f6f413dfc235f4
sabashahab <sabashahab@users.noreply.github.com>
2018-08-31 10:33:56 -0400

    Add files via upload

---
+++ Slicer_Tractography_Aug2018.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+source /etc/profile.d/modules.sh
+source /etc/profile.d/quarantine.sh
+module load slicer/0,nightly
+module load FSL/5.0.7 
+module load DTIPrep/1.2.4
+module load UKFTractography/2015-02-05
+
+
+if [ -z "$3" ]; then
+cat <<EOF
+Runs DTIPrep and UKFTractography on a DWI image
+Usage:
+  $0 <dwi> <DTIPrep-protocol.xml> <outputdir> <seeds>
+EOF
+  exit 1
+fi 
+
+
+
+inputimage=$1           # input NRRD DWI image
+dtiprep_protocol=$2     # xml spec for DTIPrep
+outputfolder=$3         # output folder for all outputs
+ 
+
+
+
+# Change the commented out section below depending on whether you are using nrrd or nhdr.
+stem=$(basename $inputimage .nrrd)
+#stem=$(basename $inputimage .nhdr)
+
+threads=8
+
+#DTI PREP
+# Run image through DTIPrep to clean up the image
+if [ ! -e $outputfolder/${stem}_QCed.nrrd ]; then
+  DTIPrep \
+    --DWINrrdFile $inputimage \
+    --xmlProtocol $dtiprep_protocol \
+    --outputFolder $outputfolder \
+    --numberOfThreads $threads \
+    --check 
+else 
+  echo "DTIPrep was already run on this subject!"
+fi
+
+#Diffusion Weighted Volume Masking 
+
+if [ ! -e $outputfolder/${stem}_MASK.nrrd ]; then
+  DiffusionWeightedVolumeMasking \
+    --removeislands \
+    ${outputfolder}/${stem}_QCed.nrrd \
+    ${outputfolder}/${stem}_SCALAR.nrrd \
+    ${outputfolder}/${stem}_MASK.nrrd
+else 
+  echo "DiffusionWeightedVolumeMasking was already run on this subject!"
+fi
+
+# Resample Mask using QCed.nrrd as the reference
+if [ ! -e $outputfolder/${stem}_MASK_oriented.nrrd ]; then
+  ResampleScalarVectorDWIVolume -R ${outputfolder}/${stem}_QCed.nrrd ${outputfolder}/${stem}_MASK.nrrd ${outputfolder}/${stem}_MASK_oriented.nrrd
+else
+  echo "ResampleScalarVectorDWIVolume(MASK) was already run on this subject!"
+fi
+
+# Resample Scalar using QCed.nrrd as the reference
+if [ ! -e $outputfolder/${stem}_SCALAR_oriented.nrrd ]; then
+  ResampleScalarVectorDWIVolume -R ${outputfolder}/${stem}_QCed.nrrd ${outputfolder}/${stem}_SCALAR.nrrd ${outputfolder}/${stem}_SCALAR_oriented.nrrd
+else
+  echo "ResampleScalarVectorDWIVolume(SCALAR) was already run on this subject!"
+fi
+
+#DWI to DTI Estimation
+
+if [ ! -e $outputfolder/${stem}_DTI.nrrd ]; then
+  DWIToDTIEstimation \
+    --enumeration WLS \
+    --shiftNeg \
+    -m $outputfolder/${stem}_MASK_oriented.nrrd \
+    $outputfolder/${stem}_QCed.nrrd \
+    $outputfolder/${stem}_DTI.nrrd \
+    $outputfolder/${stem}_SCALAR_oriented.nrrd
+else 
+  echo "DWIToDTIEstimate was already run on this subject!"
+fi
+
+#Tractography label map seeding
+
+if [ ! -e $outputfolder/${stem}_SlicerTractography.vtk ]; then
+TractographyLabelMapSeeding \
+	${outputfolder}/${stem}_DTI.nrrd \
+	${outputfolder}/${stem}_SlicerTractography.vtk \
+	--useindexspace \
+	--stoppingvalue 0.10
+else 
+  echo "TractographyLabelMapSeeding was already run on this subject!"
+fi
+
+
+
+
+
+
---
+++ wma_applyToSubject_Aug2018.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+source /etc/profile.d/modules.sh
+source /etc/profile.d/quarantine.sh
+module load python/2.7.8-anaconda-2.1.0
+module load python-extras/2.7.8
+module load whitematteranalysis/latest
+module load slicer/0,nightly
+
+if [ -z "$2" ]; then
+cat <<EOF
+
+Runs the WMA pipeline on input VTK files
+
+Usage:
+$0 <inputfile> <outputdir> <atlas> <clusteredmrml>
+
+Arguments:  
+  <inputfile>      Path to TRACTS.vtk file for subject
+  <outputdir>      Parent output dir
+  <atlas>      	   atlas.vtp file from the atlas folder
+  <clusteredmrml>  Path to the clustered mrml file
+  <tractList>  	   Path to a list of tract names
+EOF
+  exit 1
+fi 
+
+inputfolder=$1            # TRACTS.vtk file
+outputfolder=$2              # output/
+atlas=$3       	          # atlas/
+clusteredmrml=$4          # mrml file
+tractsfile=$5		  # List of tract names
+filename=`echo $1 | sed "s/.*\///" | sed "s/\..*//"`
+atlasDirectory=`dirname $atlas`
+declare -a listHemispheres=("tracts_commissural" "tracts_left_hemisphere" "tracts_right_hemisphere")
+
+mkdir -p $outputfolder
+
+if [ ! -e $outputfolder/RegisterToAtlas/$filename/output_tractography/$filename'_reg.vtk' ]; then
+wm_register_to_atlas_new.py \
+  $inputfolder $atlas $outputfolder/RegisterToAtlas 
+else 
+  echo "wm_register_to_atlas_new.py was already run on this subject!"
+fi
+
+if [ ! -e $outputfolder/ClusterFromAtlas/$filename'_reg' ]; then
+wm_cluster_from_atlas.py \
+  -l 20 \
+  $outputfolder/RegisterToAtlas/$filename/output_tractography/$filename'_reg.vtk' \
+  $atlasDirectory $outputfolder/ClusterFromAtlas
+else 
+  echo "wm_cluster_from_atlas_new.py was already run on this subject!"
+fi
+
+if [ ! -e $outputfolder/OutliersPerSubject1/$filename'_reg_outlier_removed' ]; then
+wm_cluster_remove_outliers.py \
+  -cluster_outlier_std 4 \
+  $outputfolder/ClusterFromAtlas/$filename'_reg' \
+  $atlasDirectory \
+  $outputfolder/OutliersPerSubject1
+else 
+  echo "wm_cluster_remove_outliers.py was already run on this subject!"
+fi
+
+if [ ! -e $outputfolder/ClusterByHemisphere/'OutliersPerSubject_'$filename ]; then
+wm_separate_clusters_by_hemisphere.py \
+  -atlasMRML $clusteredmrml \
+  $outputfolder/OutliersPerSubject1/$filename'_reg_outlier_removed'/ \
+  $outputfolder/ClusterByHemisphere/'OutliersPerSubject_'$filename
+else 
+  echo "wm_separate_clusters_by_hemisphere.py was already run on this subject!"
+fi
+
+if [ ! -e $outputfolder/AppendClusters/'OutliersPerSubject_'$filename ]; then
+for hemisphere in "${listHemispheres[@]}"; do
+echo $hemisphere
+while read tractname; do 
+wm_append_clusters.py \
+  -appendedTractName $tractname \
+  -tractMRML $atlasDirectory/$tractname'.mrml' \
+  $outputfolder/ClusterByHemisphere/'OutliersPerSubject_'$filename/$hemisphere \
+  $outputfolder/AppendClusters/'OutliersPerSubject_'$filename/$hemisphere >> /scratch/saba/log_wma.txt
+done < $tractsfile
+done
+else
+  echo "wm_separate_clusters_by_hemisphere.py was already run on this subject!"
+fi
+
+if [ ! -e $outputfolder/FiberMeasurements/$filename ]; then
+for hemisphere in "${listHemispheres[@]}"; do
+echo $hemisphere
+mkdir -p $outputfolder/FiberMeasurements/$filename/$hemisphere/
+echo $outputfolder/FiberMeasurements/$filename/$hemisphere >> /scratch/saba/testing.txt; 
+FiberTractMeasurements \
+  --outputfile $outputfolder/FiberMeasurements/$filename/$hemisphere/$filename'.csv' \
+  --inputdirectory $outputfolder/AppendClusters/'OutliersPerSubject_'$filename/$hemisphere \
+  -i Fibers_File_Folder \
+  --separator Tab \
+  -f Column_Hierarchy
+done
+else
+  echo "FiberTractMeasurements was already run on this subject!"
+fi
+
